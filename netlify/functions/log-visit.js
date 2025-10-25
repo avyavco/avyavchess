cd54daba7967917ded5a53d56d1e12594148763b
avyavco <doppaioppai@gmail.com>
2025-10-25 10:33:58 +0545

    Update log-visit.js

--- netlify/functions/log-visit.js
+++ netlify/functions/log-visit.js
@@ -1,1 +1,15 @@
+exports.handler = async (event, context) => {
+  // Get visitor IP from request headers
+  const ip = event.headers['x-forwarded-for'] || event.ip || 'Unknown IP';
+  const userAgent = event.headers['user-agent'] || 'Unknown agent';
+
+  console.log(`New visit from ${ip} (${userAgent})`);
+
+  // You can expand this to write to a database, file, or API later
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Visit logged", ip }),
+  };
+};
 
